afd6bfc47dbc9fdd3b7af4ce4aa7ae2a4ed04eaf
Totobird <totobird@noreply.codeberg.org>
2026-06-06 09:23:01 -0400

    feat(ff-filter): WIP FilterName impls

--- crates/ff-filter/src/graph/filter_name.rs
+++ crates/ff-filter/src/graph/filter_name.rs
@@ -1,3 +1,94 @@
+use crate::{BlendMode, ScaleAlgorithm, ToneMap, XfadeTransition, YadifMode};
+
 pub trait FilterName {
     fn filter_name(&self) -> &'static str;
+}
+
+impl FilterName for ScaleAlgorithm {
+    fn filter_name(&self) -> &'static str {
+        // https://ffmpeg.org/ffmpeg-filters.html#Scaling
+        match self {
+            Self::Fast => "fast_bilinear",
+            Self::Bilinear => "bilinear",
+            Self::Bicubic => "bicubic",
+            Self::Lanczos => "lanczos",
+        }
+    }
+}
+
+impl FilterName for ToneMap {
+    fn filter_name(&self) -> &'static str {
+        // https://ffmpeg.org/ffmpeg-filters.html#Tone-mapping
+        match self {
+            Self::Hable => "hable",
+            Self::Reinhard => "reinhard",
+            Self::Mobius => "mobius",
+        }
+    }
+}
+
+impl FilterName for YadifMode {
+    fn filter_name(&self) -> &'static str {
+        // https://ffmpeg.org/ffmpeg-filters.html#yadif-1
+        match self {
+            Self::Frame => "0",
+            Self::Field => "1",
+            Self::FrameNospatial => "2",
+            Self::FieldNospatial => "3",
+        }
+    }
+}
+
+impl FilterName for XfadeTransition {
+    fn filter_name(&self) -> &'static str {
+        // https://ffmpeg.org/ffmpeg-filters.html#xfade
+        match self {
+            Self::Dissolve => "dissolve",
+            Self::Fade => "fade",
+            Self::WipeLeft => "wipeleft",
+            Self::WipeRight => "wiperight",
+            Self::WipeUp => "wipeup",
+            Self::WipeDown => "wipedown",
+            Self::SlideLeft => "slideleft",
+            Self::SlideRight => "slideright",
+            Self::SlideUp => "slideup",
+            Self::SlideDown => "slidedown",
+            Self::CircleOpen => "circleopen",
+            Self::CircleClose => "circleclose",
+            Self::FadeGrays => "fadegrays",
+            Self::Pixelize => "pixelize",
+        }
+    }
+}
+
+impl FilterName for BlendMode {
+    fn filter_name(&self) -> &'static str {
+        // https://ffmpeg.org/ffmpeg-filters.html#blend-1
+        match self {
+            Self::Normal => "normal",
+            Self::Multiply => "multiply",
+            Self::Screen => "screen",
+            Self::Overlay => "overlay",
+            Self::SoftLight => "softlight",
+            Self::HardLight => "hardlight",
+            Self::ColorDodge => "dodge",
+            Self::ColorBurn => "burn",
+            Self::Darken => "darken",
+            Self::Lighten => "lighten",
+            Self::Difference => "difference",
+            Self::Exclusion => "exclusion",
+            Self::Add => "addition",
+            Self::Subtract => "subtract",
+            Self::Hue => todo!(),
+            Self::Saturation => todo!(),
+            Self::Color => todo!(),
+            Self::Luminosity => todo!(),
+            Self::PorterDuffOver => todo!(),
+            Self::PorterDuffUnder => todo!(),
+            Self::PorterDuffIn => todo!(),
+            Self::PorterDuffOut => todo!(),
+            Self::PorterDuffAtop => todo!(),
+            Self::PorterDuffXor => todo!(),
+        }
+    }
 }
